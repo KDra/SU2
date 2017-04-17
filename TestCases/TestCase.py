08a040cdd8016072eda0234d18aff93465848611
K. Drakopoulos <drakopuloskostas@gmail.com>
2017-04-18 02:51:19 +0300

    TestCase.py merge comment fixed

--- TestCases/TestCase.py
+++ TestCases/TestCase.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env python 
-
-## \file TestCase.py
-#  \brief Python class for automated regression testing of SU2 examples
-#  \author A. Aranake, A. Campos, T. Economon, T. Lukaczyk, S. Padron
-#  \version 4.3.0 "Cardinal"
-#
-# SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
-#                      Dr. Thomas D. Economon (economon@stanford.edu).
-#
-# SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
-#                 Prof. Piero Colonna's group at Delft University of Technology.
-#                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
-#                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
-#                 Prof. Rafael Palacios' group at Imperial College London.
-#                 Prof. Edwin van der Weide's group at the University of Twente.
-#                 Prof. Vincent Terrapon's group at the University of Liege.
-#
-# Copyright (C) 2012-2016 SU2, the open-source CFD code.
-#
 # SU2 is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
@@ -1397,4 +1376,3 @@ class TestCase:
         os.chdir(workdir)
 
         return
->>>>>>> upstream/master
